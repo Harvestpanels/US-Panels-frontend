84382b39004dfd80b942252886d3b791345dbbf9
0xhighstandards <0xhighstandards@gmail.com>
2026-09-09 22:36:49 +0800

    update responsive screen

--- src/data/site.js
+++ src/data/site.js
@@ -6,7 +6,7 @@ export const PARALLAX_BG_URL = facilityImg;
 
 export const CONTACT = {
   address: "5920 Campbell Ln, Piedmont, Oklahoma 73078, United States",
-  phone: "(405) 723-1220",
-  phoneHref: "4057231220",
+  phone: "(405) 778-2808",
+  phoneHref: "4057782808",
   email: "Sales@uspanels.com",
 };
